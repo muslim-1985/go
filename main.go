9634a268c3fe0bd89623ef2e4847bef03ab7cec7
muslim <muslim.krum.85@gmail.com>
2019-03-25 20:33:06 +0200

    first committ

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	a := App{}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
+	    a.Run("localhost:8080")
+}
